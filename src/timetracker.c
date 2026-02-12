85be06c3d94eff6b166b1007c093c713d413cdb6
AndrewCRMartin <andrew@bioinf.org.uk>
2026-02-12 11:32:31 +0000

    Added colours with CSS

--- src/timetracker.c
+++ src/timetracker.c
@@ -65,11 +65,73 @@ int main(int argc, char **argv);
 static void logtime(int state, time_t theTime);
 static void output_state(GtkToggleButton *source, gpointer user_data);
 static void activate(GtkApplication *app, gpointer user_data);
+static void ReadCSS(char *cssFile);
 
 
+/************************************************************************/
+int main(int argc, char **argv)
+{
+   GtkApplication *app;
+   int status = 1;
+   char *fnm = "TimeTrackerRecord.csv";
+   
+   /* Open the tracking file (append)                                   */
+   if((gFpRecord = fopen(fnm, "a"))==NULL)
+   {
+      fprintf(stderr, "Unable to open time tracking file: %s\n", fnm);
+   }
+   else
+   {
+      /* Read CSS info for colours                                      */
+      gtk_init(&argc, &argv);
+      ReadCSS("timetracker.css");
+      
+      /* Create and name the application                                */
+      app = gtk_application_new("org.acrm.timetracker",
+                                G_APPLICATION_DEFAULT_FLAGS);
+      
+      /* Connect the activate signal to the activate function           */
+      g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+
+      /* Run the application which will send the activate signal        */
+      status = g_application_run(G_APPLICATION(app), argc, argv);
+      
+      /* Free up the application resources                              */
+      g_object_unref(app);
+      
+      /* Log that we have closed the application                        */
+      fprintf(gFpRecord,"\"CLOSED\",,\n");
+      fclose(gFpRecord);
+   }
+   return status;
+}
 
 
-/***********************************************************************/
+/************************************************************************/
+static void ReadCSS(char *cssFile)
+{
+   GtkCssProvider *provider;
+   GdkDisplay     *display;
+   GdkScreen      *screen;
+   GError         *error = 0;
+
+   provider = gtk_css_provider_new();
+   display  = gdk_display_get_default();
+   screen   = gdk_display_get_default_screen(display);
+   gtk_style_context_add_provider_for_screen(screen,
+             GTK_STYLE_PROVIDER(provider),
+             GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+   gtk_css_provider_load_from_file(provider,
+                                   g_file_new_for_path(cssFile),
+                                   &error);
+   g_object_unref(provider);
+
+   if(error)
+      printf("Error reading CSS file: %s\n", cssFile);
+}
+
+
+/************************************************************************/
 static void logtime(int state, time_t theTime)
 {
    char *tString;
@@ -97,86 +159,54 @@ static void logtime(int state, time_t theTime)
 }
 
 
-/***********************************************************************/
+/************************************************************************/
 static void output_state(GtkToggleButton *source, gpointer user_data)
 {
-  if(gtk_toggle_button_get_active(source))
-  {
-     gtk_button_set_label(GTK_BUTTON(source), (gchar *)"Stop");
-     time(&gStartTime);
-     logtime(1, gStartTime);
-  }
-  else
-  {
-     time_t stopTime;
-     time(&stopTime);
-     gTotalTime += (stopTime - gStartTime);
-     logtime(0, stopTime);
-     gtk_button_set_label(GTK_BUTTON(source), (gchar *)"Start");
-  }
+   if(gtk_toggle_button_get_active(source))
+   {
+      gtk_button_set_label(GTK_BUTTON(source), (gchar *)"Stop");
+      gtk_widget_set_name(GTK_WIDGET(source), "toggle_red");
+
+      time(&gStartTime);
+      logtime(1, gStartTime);
+   }
+   else
+   {
+      time_t stopTime;
+      time(&stopTime);
+      gTotalTime += (stopTime - gStartTime);
+      logtime(0, stopTime);
+      gtk_button_set_label(GTK_BUTTON(source), (gchar *)"Start");
+      gtk_widget_set_name(GTK_WIDGET(source), "toggle_green");
+   }
 }
 
 
-/***********************************************************************/
+/************************************************************************/
 static void activate(GtkApplication *app, gpointer user_data)
 {
-  GtkWidget *window;
-  GtkWidget *box, *toggle;
-
-  /* Create a window widget with title and size                        */
-  window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window), "Time Tracker");
-  gtk_window_set_default_size(GTK_WINDOW(window), 100, 50);
-
-  box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 12);
-
-  toggle = gtk_toggle_button_new_with_label("Start");
-
-  /* Makes this toggle button visible                                  */
-  gtk_toggle_button_set_mode(GTK_TOGGLE_BUTTON(toggle), FALSE);
-
-  /* Call the output_state() function when button is toggled           */
-  g_signal_connect(toggle, "toggled", G_CALLBACK(output_state), NULL);
-
-  /* Add the toggle to the box and then the box to the window          */
-  gtk_container_add(GTK_CONTAINER(box), toggle);
-  gtk_container_add(GTK_CONTAINER(window), box);
-
-  /* Show all the widgets                                              */
-  gtk_widget_show_all(window);
+   GtkWidget *window;
+   GtkWidget *box, *toggle;
+   
+   /* Create a window widget with title and size                        */
+   window = gtk_application_window_new(app);
+   gtk_window_set_title(GTK_WINDOW(window), "Time Tracker");
+   gtk_window_set_default_size(GTK_WINDOW(window), 100, 50);
+   
+   box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 12);
+   
+   toggle = gtk_toggle_button_new_with_label("Start");
+   gtk_widget_set_name(toggle, "toggle_green");
+   
+   /* Call the output_state() function when button is toggled           */
+   g_signal_connect(toggle, "toggled", G_CALLBACK(output_state), NULL);
+   
+   /* Add the toggle to the box and then the box to the window          */
+   gtk_container_add(GTK_CONTAINER(box), toggle);
+   gtk_container_add(GTK_CONTAINER(window), box);
+   
+   /* Show all the widgets                                              */
+   gtk_widget_show_all(window);
 }
 
 
-/***********************************************************************/
-int main(int argc, char **argv)
-{
-  GtkApplication *app;
-  int status = 1;
-  char *fnm = "TimeTrackerRecord.csv";
-
-  /* Open the tracking file (append)                                   */
-  if((gFpRecord = fopen(fnm, "a"))==NULL)
-  {
-     fprintf(stderr, "Unable to open time tracking file: %s\n", fnm);
-  }
-  else
-  {
-     /* Create and name the application                                */
-     app = gtk_application_new("org.acrm.timetracker",
-                                G_APPLICATION_DEFAULT_FLAGS);
-     
-     /* Connect the activate signal to the activate function           */
-     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-     
-     /* Run the application which will send the activate signal        */
-     status = g_application_run(G_APPLICATION(app), argc, argv);
-     
-     /* Free up the application resources                              */
-     g_object_unref(app);
-
-     /* Log that we have closed the application                        */
-     fprintf(gFpRecord,"\"CLOSED\",,\n");
-     fclose(gFpRecord);
-  }
-  return status;
-}
